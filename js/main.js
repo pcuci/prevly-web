354a09bc2162686afca34bfbfdfc9b92ea97a9e6
Paul Cuciureanu <paul@pplconnect.mobi>
2014-03-30 16:42:09 -0400

    tracking modal close button. Google universal analytics events are setup via jQuery, in js/main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@
 $('#call-to-action-button').on('click', function() {
   ga('send', 'event', 'button', 'click', 'call-to-action-button');
 });
+
+$('#close-questionnaire-button').on('click', function() {
+  ga('send', 'event', 'button', 'click', 'close-questionnaire-button');
+});
