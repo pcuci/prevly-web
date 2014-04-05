00192c2e5a99f2ffd68000ab46d14a29725ba789
Paul Cuciureanu <paul@pplconnect.mobi>
2014-04-05 16:08:28 -0400

    updating about us page copy

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ $('#call-to-action-button').on('click', function() {
   ga('send', 'event', 'button', 'click', 'call-to-action-button');
 });
 
+$('#call-to-action-download-sana-app-btn').on('click', function() {
+  ga('send', 'event', 'button', 'click', 'call-to-action-download-sana-app-btn');
+});
+
 $('#close-questionnaire-button').on('click', function() {
   ga('send', 'event', 'button', 'click', 'close-questionnaire-button');
 });
